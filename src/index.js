45847f6adb92f041b57aff98e951dbb2c3c81791
wantboard <info@wantboard.com>
2023-06-12 21:05:50 +0000

    Logging contents of CSS file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chatbot from './components/chatbot';
-import './styles.css';
+import styles from './styles.css';
+
+console.log(styles);
 
 ReactDOM.render(
     <Chatbot />,
-document.getElementById('root')
+    document.getElementById('root')
 );
